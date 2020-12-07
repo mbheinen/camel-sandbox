87ed651cea7e5893f1b240624cecf1ae7531e031
Mike Heinen <mbheinen@gmail.com>
2020-12-07 11:49:19 -0600

    Update paths and added comments
    Update paths and added comments

--- src/main/java/com/SimpleRouteBuilder.java
+++ src/main/java/com/SimpleRouteBuilder.java
@@ -2,11 +2,13 @@ package com;
 
 import org.apache.camel.builder.RouteBuilder;
 
+/* Simiple Route for copying files from one directory to another.
+ */
 public class SimpleRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("file:C:/Users/gwenm/mike/inputFiles?noop=true").to("file:C:/Users/gwenm/mike/outputFiles");
+        from("file:C:/inputFiles?noop=true").to("file:C:/outputFiles");
     }
 
 }
